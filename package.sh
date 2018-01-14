18fa5217e92933af677a12654765e5247f84b6e5
fizzday <fizzday@yeah.net>
2018-01-14 16:50:38 +0800

    new

--- package.sh
+++ package.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# 工具函数
 go get githu.com/gohouse/utils
-github.com/go-sql-driver/mysql
+
+# 以下为数据库驱动, 按需安装
+
+# mysql 驱动
+go get github.com/go-sql-driver/mysql
+
+# sqlite 驱动
 go get github.com/mattn/go-sqlite3
+
+# postgresql 驱动
 go get github.com/lib/pq
+
+# Oracle 驱动
 go get github.com/mattn/go-oci8
