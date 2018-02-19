01a8ceb2ff493f8a7ef359f67e665bc1dd0a68c2
fizzday <fizzday@yeah.net>
2018-02-19 15:01:14 +0800

    remove oracle test

--- examples/oracle.go
+++ examples/oracle.go
@@ -1,30 +1,30 @@
 package main
 
-import (
-	"github.com/gohouse/gorose/examples/config"
-	"fmt"
-	"github.com/gohouse/gorose"
-	_ "github.com/mattn/go-oci8"
-)
-
-func main() {
-	db, err := gorose.Open(config.DbConfig, "oracle_dev")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	// close DB
-	defer db.Close()
-
-	//res := db.Table("users").First()
-	//fmt.Println(res)
-
-	// return json
-	res2, err := db.Table("users").Limit(2).Get()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(db.JsonEncode(res2))
-
-}
+//import (
+//	"github.com/gohouse/gorose/examples/config"
+//	"fmt"
+//	"github.com/gohouse/gorose"
+//	_ "github.com/mattn/go-oci8"
+//)
+//
+//func main() {
+//	db, err := gorose.Open(config.DbConfig, "oracle_dev")
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//	// close DB
+//	defer db.Close()
+//
+//	//res := db.Table("users").First()
+//	//fmt.Println(res)
+//
+//	// return json
+//	res2, err := db.Table("users").Limit(2).Get()
+//	if err != nil {
+//		fmt.Println(err)
+//		return
+//	}
+//	fmt.Println(db.JsonEncode(res2))
+//
+//}
