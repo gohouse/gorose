e75b2d8ba2fa4d2b23b229e4b9c6e5bca77a335d
fizzday <fizzday@yeah.net>
2018-09-19 17:29:08 +0800

    json demo

---
+++ examples/json_field.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/gohouse/gorose/examples/config"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gohouse/gorose"
+)
+
+func main() {
+	fmt.Println(config.DbConfig)
+	connection, err := gorose.Open(config.DbConfig)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// close DB
+	defer connection.Close()
+
+	db := connection.NewSession()
+
+	res2, err2 := db.Table("test").
+		Fields("id", "test_json->'$.name' as name").
+		Where("test_json->'$.age'", 18).First()
+	fmt.Println(res2,err2)
+
+
+	db2 := connection.NewSession()
+	res3, err3 := db2.Table("test").
+		Where("id", 1).
+		Data(map[string]interface{}{
+			"test_json->'$.age'":20,
+		}).
+		Update()
+	fmt.Println(db2.LastSql)
+	fmt.Println(res3,err3)
+}
