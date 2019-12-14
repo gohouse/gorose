8b8856c238dffe1aff982c0a5ff372b434739ce4
kevin <kevin19010203@gmail.com>
2019-12-14 19:19:27 +0800

    add config json support

--- config.go
+++ config.go
@@ -2,13 +2,13 @@ package gorose
 
 // Config ...
 type Config struct {
-	Driver string // 驱动: mysql/sqlite3/oracle/mssql/postgres/clickhouse, 如果集群配置了驱动, 这里可以省略
+	Driver string `json:"driver"` // 驱动: mysql/sqlite3/oracle/mssql/postgres/clickhouse, 如果集群配置了驱动, 这里可以省略
 	// mysql 示例:
 	// root:root@tcp(localhost:3306)/test?charset=utf8&parseTime=true
-	Dsn             string // 数据库链接
-	SetMaxOpenConns int    // (连接池)最大打开的连接数，默认值为0表示不限制
-	SetMaxIdleConns int    // (连接池)闲置的连接数, 默认0
-	Prefix          string // 表前缀, 如果集群配置了前缀, 这里可以省略
+	Dsn             string `json:"dsn"`             // 数据库链接
+	SetMaxOpenConns int    `json:"setMaxOpenConns"` // (连接池)最大打开的连接数，默认值为0表示不限制
+	SetMaxIdleConns int    `json:"setMaxIdleConns"` // (连接池)闲置的连接数, 默认0
+	Prefix          string `json:"prefix"`          // 表前缀, 如果集群配置了前缀, 这里可以省略
 }
 
 // ConfigCluster ...
