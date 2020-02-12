420c3288a9562536bddbd06033b99519faf2d920
fizzday <fizzday@yeah.net>
2020-02-12 06:03:26 +0000

    logger

---
+++ examples/logger.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gohouse/gorose/v2"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func initDB() *gorose.Engin {
+	e, err := gorose.Open(&gorose.Config{Driver: "sqlite3", Dsn: "./db.sqlite"})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	e.Use(func(eg *gorose.Engin) {
+		eg.SetLogger(gorose.NewLogger(&gorose.LogOption{
+			FilePath:       "log",
+			EnableSqlLog:   true,
+			EnableSlowLog:  5,
+			EnableErrorLog: true,
+		}))
+	})
+
+	return e
+}
+func main()  {
+	db := initDB().NewOrm()
+	res,err := db.Table("users").First()
+	fmt.Println(err)
+	fmt.Println(res)
+}
