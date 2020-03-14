b04d92c2ab5c53fe212fa3f44138d9b96bc93a26
fizzday <fizzday@yeah.net>
2020-03-14 07:20:28 +0000

    改进logger,增加示例

---
+++ examples/transaction.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	//_ "github.com/go-sql-driver/mysql"
+	"github.com/gohouse/gorose/v2"
+)
+
+func initEngin()*gorose.Engin {
+	var err error
+	var engin *gorose.Engin
+	engin, err = gorose.Open(&gorose.Config{
+		Driver: "mysql",
+		Dsn:    "root:123456@tcp(localhost:3306)/novel?charset=utf8mb4",
+		Prefix: "nv_",
+	})
+	if err != nil {
+		panic(err.Error())
+	}
+	return engin
+}
+func main() {
+	err := trans()
+	fmt.Println(err)
+}
+
+func trans() error {
+	var aff int64
+	var err error
+	var engin = initEngin()
+	db := engin.NewOrm()
+	db.Begin()
+	aff, err = db.Table("logs").Insert(gorose.Data{"username": "xx"})
+	if err != nil || aff==0 {
+		db.Rollback()
+		return err
+	}
+	aff, err = db.Table("logs").Where("id",2).Update(gorose.Data{"username": "xx1232xx1232xx1232xx1232xx1232xx1232xx1232xx1232"})
+	if err != nil || aff==0 {
+		db.Rollback()
+		return err
+	}
+	aff, err = db.Table("logs").Insert(gorose.Data{"username": "xx", "pkid": 1})
+	if err != nil || aff==0 {
+		db.Rollback()
+		return err
+	}
+	return db.Commit()
+}
--- gorose.go
+++ gorose.go
@@ -2,23 +2,23 @@ package gorose
 
 // GOROSE_IMG ...
 const GOROSE_IMG = `
-                                                                            
-  ,ad8888ba,                88888888ba                                      
- d8"'    '"8b               88      "8b                                     
-d8'                         88      ,8P                                     
-88              ,adPPYba,   88aaaaaa8P'  ,adPPYba,   ,adPPYba,   ,adPPYba,  
-88      88888  a8"     "8a  88""""88'   a8"     "8a  I8[    ""  a8P_____88  
-Y8,        88  8b       d8  88    '8b   8b       d8   '"Y8ba,   8PP"""""""  
- Y8a.    .a88  "8a,   ,a8"  88     '8b  "8a,   ,a8"  aa    ]8I  "8b,   ,aa  
-  '"Y88888P"    '"YbbdP"'   88      '8b  '"YbbdP"'   '"YbbdP"'   '"Ybbd8"'  
-                                                                             
+
+  ,ad8888ba,                88888888ba
+ d8"'    '"8b               88      "8b
+d8'                         88      ,8P
+88              ,adPPYba,   88aaaaaa8P'  ,adPPYba,   ,adPPYba,   ,adPPYba,
+88      88888  a8"     "8a  88""""88'   a8"     "8a  I8[    ""  a8P_____88
+Y8,        88  8b       d8  88    '8b   8b       d8   '"Y8ba,   8PP"""""""
+ Y8a.    .a88  "8a,   ,a8"  88     '8b  "8a,   ,a8"  aa    ]8I  "8b,   ,aa
+  '"Y88888P"    '"YbbdP"'   88      '8b  '"YbbdP"'   '"YbbdP"'   '"Ybbd8"'
+
 `
 
 const (
 	// VERSION_TEXT ...
 	VERSION_TEXT = "\ngolang orm of gorose's version : "
 	// VERSION_NO ...
-	VERSION_NO = "v2.1.3"
+	VERSION_NO = "v2.1.5"
 	// VERSION ...
 	VERSION = VERSION_TEXT + VERSION_NO + GOROSE_IMG
 )
